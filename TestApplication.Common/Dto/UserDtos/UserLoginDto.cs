77baa24f616cfc2fd386a7abbcc7c2e0d8b3e2d3
DenizUzundag <uzundagdeniz@gmail.com>
2020-12-24 22:43:17 +0300

    data annotation

--- TestApplication.Common/Dto/UserDtos/UserLoginDto.cs
+++ TestApplication.Common/Dto/UserDtos/UserLoginDto.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TestApplication.Common.Dto.UserDtos
 {
     public class UserLoginDto
     {
+        [Required(ErrorMessage = "Email alanı boş geçilemez")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "PassWord alanı boş geçilemez")]
         public string Password { get; set; }
     }
 }
